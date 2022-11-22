20278802918fa1341df1e64c1257121e88b4794e
Isaac Roldán <isaac.roldan@shopify.com>
2022-11-22 17:38:32 +0100

    Update CLI

--- extensions/consolidated-projection-ext/src/index.tsx
+++ extensions/consolidated-projection-ext/src/index.tsx
@@ -11,7 +11,7 @@ function App(root, { extensionPoint }) {
     root.createComponent(
       Text,
       {},
-      `Welcome to the ${extensionPoint} extension!`
+      `Welcome to the ${extensionPoint} extension!!!!`
     )
   );
   root.mount();
